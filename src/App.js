c57b4a610866411e4977d4c0c03a341d70cdbf76
Hein Soe <35089574+hheinsoee@users.noreply.github.com>
2021-10-11 10:56:31 +0630

    updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import a_photo from './demo-backend/profile/cover.jpg'
 import ItemThumbnail from './template/item';
-import { Drawer } from '@mui/material';
+import { Drawer, Hidden, SwipeableDrawer, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ShareSharpIcon from '@mui/icons-material/ShareSharp';
 
 // နမူနာ api
-const shop = { 
+const shop = {
   name: "Food Shop",
   description: "best place in thw world",
   feature_image: a_photo,
@@ -28,7 +30,7 @@ function App() {
             background: `linear-gradient(transparent, ${shop.theme_color})`
           }}
         >
-          <h1>{shop.name}</h1>
+          <h1>{shop.name}&nbsp;<CheckCircleIcon /></h1>
           <p>{shop.description}</p>
         </div>
       </header>
@@ -49,10 +51,13 @@ function Items() {
   });
 
   const toggleDrawer = (item_id, isopen) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
-
     setState({
       item_id: item_id,
       isopen: isopen
@@ -63,28 +68,41 @@ function Items() {
     <Box
       role="presentation"
       onKeyDown={toggleDrawer(item_id, false)}
+      sx={{
+        p: 2,
+      }}
     >
-      {item_id}
+      <Typography gutterBottom variant="h5" component="div">
+        {item_id}<ShareSharpIcon />
+      </Typography>
+      {item_id}<br />
+      {item_id}<br />
+      {item_id}<br />
+      {item_id}<br />
+      {item_id}<br />
     </Box>
   );
 
   return (
-      <React.Fragment className="_my">
-        {/* loop ရန် အစ*/}
-        {['ဝက်သား', 'ကြက်သား', 'ဆိတ်သား'].map((item) => (
+    <React.Fragment className="_my">
+      {/* loop ရန် အစ*/}
+      {['ဝက်သား', 'ကြက်သား', 'ဆိတ်သား', 'အမဲသား', 'ဆတ်သား'].map((item) => (
         <div key={item} onClick={toggleDrawer(item, true)}>
           <ItemThumbnail item={item} />
         </div>
-        ))}
-        {/* loop ရန် အဆုံး*/}
+      ))}
+      {/* loop ရန် အဆုံး*/}
 
-        <Drawer
-          anchor='bottom'
-          open={state.isopen}
-          onClose={toggleDrawer(state.item_id, false)}
-        >
-          {item_detail(state.item_id)}
-        </Drawer>
-      </React.Fragment>
+      <br />
+
+      <SwipeableDrawer
+        anchor='bottom'
+        open={state.isopen}
+        onClose={toggleDrawer(state.item_id, false)}
+        onOpen={toggleDrawer(state.item_id, true)}
+      >
+        {item_detail(state.item_id)}
+      </SwipeableDrawer>
+    </React.Fragment>
   );
 }
